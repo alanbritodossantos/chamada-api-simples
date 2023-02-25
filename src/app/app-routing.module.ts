6e431dabb089ddf93e92dbd1fa537e7361b20bd0
Alan <alanbritodossantos2015@gmail.com>
2023-02-25 20:12:31 -0300

    foi realizado a chamada de api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { UserListComponent } from './user-list/user-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/users',pathMatch:'full'},
+  {path:'users',component:UserListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: any[] =[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any>('https://randomuser.me/api/?results=10').subscribe(data => {
+      this.users = data.results;
+    });
+  }
+
+}
